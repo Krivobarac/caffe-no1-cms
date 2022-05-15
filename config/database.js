215c04947a2d3c5828798f364ea572cf8c0654c9
Krivobarac <gasparovicsasa@yahoo.com>
2022-05-15 21:11:14 +0200

    update local db connection

--- config/database.js
+++ config/database.js
@@ -6,8 +6,8 @@ module.exports = ({ env }) => ({
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'caffe_no1_db'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'postgres'),
-      ssl: env.bool('DATABASE_SSL', false),
+      password: env('DATABASE_PASSWORD', 'ojmp@83GS'),
+      ssl: env.bool('DATABASE_SSL', false)
     },
   },
 });

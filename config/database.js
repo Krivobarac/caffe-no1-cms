960e801db0eebd769c5bc166d23b80440662f595
Saša Gašparović <33967593+Krivobarac@users.noreply.github.com>
2023-04-10 14:40:36 +0200

    removed db public credentials

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'ec2-18-215-96-22.compute-1.amazonaws.com'),
+      host: env('DATABASE_HOST', '***'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'dc5k4annp8f9o'),
-      user: env('DATABASE_USERNAME', 'gagqgceeqpdgfw'),
-      password: env('DATABASE_PASSWORD', '2af021deeab2ddd8a0cc128f923acac1c072fad056b522c28d5b1df50424315b'),
+      database: env('DATABASE_NAME', '****'),
+      user: env('DATABASE_USERNAME', '****'),
+      password: env('DATABASE_PASSWORD', '****'),
       ssl: env.bool('DATABASE_SSL', false)
     },
   },
